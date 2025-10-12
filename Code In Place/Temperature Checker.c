9330bfa931375d28abac546d31258b8f2f75bf3d
aareeba2006 <aareeba2006@gmail.com>
2025-10-12 05:04:52 -0700

    Add files via upload

---
+++ Code In Place/Temperature Checker.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    //Temperature checker
+    int temperature;
+    printf("Temperature is : ");
+    scanf("%d" , &temperature);
+
+    if("Temperature > 30"){
+        printf("Weather is hot");
+    }
+    
+    
+    return 0;
+
+}
