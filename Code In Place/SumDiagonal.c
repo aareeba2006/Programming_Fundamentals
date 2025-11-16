7b14307e88efefca6eb1d2636db5e22b4cede8b9
aareeba2006 <aareeba2006@gmail.com>
2025-11-16 23:22:50 +0500

    Add files via upload

---
+++ Code In Place/SumDiagonal.c
@@ -0,0 +1,15 @@
+//Sum of a Diagonal
+#include<stdio.h>
+int main(){
+   
+    int arr[3][3] = { {1,2,3}, {4,5,6}, {7,8,9} };
+    int sum = 0;
+
+    for(int i=0; i<3; i++){
+        sum = sum + arr[i][i];   // diagonal means row index = column index
+    }
+
+    printf("Sum of diagonal = %d", sum);
+
+     return 0;
+}
