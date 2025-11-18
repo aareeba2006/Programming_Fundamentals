07d615c492801d2efe9e8ac494a3974f449d59cb
aareeba2006 <aareeba2006@gmail.com>
2025-11-19 01:05:21 +0500

    Add files via upload

---
+++ Code In Place/CourseInfo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+   //Function with no arguments and no return value
+
+    void printCourseInfo(){
+    printf("Course: Programming Fundamentals-Learn, Code, Grow!");
+ }
+
+int main(){
+     printCourseInfo();      //Calling the function from main
+     return 0;
+}
